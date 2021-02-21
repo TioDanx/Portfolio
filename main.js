d7cf9460eb95674af85c098399251c8214527ab5
Dani <yesdanlocooo@gmail.com>
2021-02-20 21:07:48 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+ScrollReveal().reveal('.welcome',{
+    duration: 2000,
+    origin: 'left',
+    distance: '400px',
+    reset: true
+})
+ScrollReveal().reveal('.fd',{
+    duration: 2000,
+    origin: 'right',
+    distance: '400px',
+    delay: '300',
+    reset: true
+})
+ScrollReveal().reveal('.photo',{
+    duration: 2000,
+    origin: 'left',
+    distance: '400px',
+    reset: true
+})
+ScrollReveal().reveal('.about-me',{
+    duration: 1000,
+    origin: 'top',
+    distance: '400px',
+    delay: '300',
+    reset: true
+})
+ScrollReveal().reveal('.my-description',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '400px',
+    delay: '600',
+    reset: true
+})
+ScrollReveal().reveal('.formation-title',{
+    duration: 2000,
+    origin: 'top',
+    distance: '200px',
+    delay: '300',
+    reset: true
+})
+ScrollReveal().reveal('.tecnica',{
+    duration: 3000,
+    origin: 'left',
+    distance: '200px',
+    delay: '300',
+    reset: true
+})
+ScrollReveal().reveal('.unmdp',{
+    duration: 2000,
+    origin: 'left',
+    distance: '200px',
+    delay: '300',
+    reset: true
+})
+ScrollReveal().reveal('.coderhouse',{
+    duration: 1000,
+    origin: 'left',
+    distance: '200px',
+    delay: '300',
+    reset: true
+})
+ScrollReveal().reveal('.technologies-title',{
+    duration: 1000,
+    origin: 'left',
+    distance: '200px',
+    delay: '300',
+    reset: true
+})
+ScrollReveal().reveal('.technologies-explanation',{
+    duration: 2000,
+    origin: 'right',
+    distance: '200px',
+    delay: '300',
+    reset: true
+})
+ScrollReveal().reveal('.svgs',{
+    duration: 2000,
+    origin: 'bottom',
+    distance: '300px',
+    delay: '300',
+    reset: true
+})
