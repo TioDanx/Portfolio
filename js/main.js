7daefcc0c7e01ef89e43b0ea05190f72d9f7b592
Dani <yesdanlocooo@gmail.com>
2021-02-23 04:09:58 -0300

    ux/ui changes

---
+++ js/main.js
@@ -0,0 +1,49 @@
+new fullpage('#fullpage', {
+    anchors: ['Presentation', 'Aboutme', 'Formation', 'Technologies', 'Projects'],
+    loopBottom: true,
+    loopTop: true,
+    keyboardScrolling: true,
+    animateAnchor: true,
+    recordHistory: true,
+    verticalCentered: false,
+    navigation: true,
+    showActiveTooltip: true,
+    scrollingSpeed: 1200,
+    scrollOverflow: true,
+    responsiveHeight: 900
+});
+
+ScrollReveal().reveal('.welcome', {
+    duration: 1000,
+    origin: 'left',
+    distance: '400px',
+    reset: true
+})
+ScrollReveal().reveal('.fd', {
+    duration: 1000,
+    origin: 'right',
+    distance: '400px',
+    delay: '500',
+    reset: true
+})
+ScrollReveal().reveal('.cv', {
+    duration: 1000,
+    origin: 'right',
+    distance: '200px',
+    delay: '1000',
+    reset: true
+})
+ScrollReveal().reveal('.github', {
+    duration: 1000,
+    origin: 'right',
+    distance: '200px',
+    delay: '1250',
+    reset: true
+})
+ScrollReveal().reveal('.linkedin', {
+    duration: 1000,
+    origin: 'right',
+    distance: '200px',
+    delay: '1500',
+    reset: true
+})
